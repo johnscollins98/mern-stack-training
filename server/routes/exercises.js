be2f47a7fd998607f4b1e4e50a59c0c537ca55d3
John Collins <johnscollins98@gmail.com>
2020-08-14 22:07:02 +0100

    Add Update/Delete for exercises

--- server/routes/exercises.js
+++ server/routes/exercises.js
@@ -26,4 +26,40 @@ router.post("/add", (req, res) => {
     .catch((err) => res.status(400).json(`Error: ${err}`));
 });
 
+router.get("/:id", (req, res) => {
+  Exercise.findById(req.params.id)
+    .then((exercise) => {
+      if (exercise == null)
+        return res.status(404).json(`Exercise ${req.params.id} not found.`);
+      res.json(exercise);
+    })
+    .catch((err) => res.status(400).json(`Error: ${err}`));
+});
+
+router.delete("/:id", (req, res) => {
+  Exercise.findByIdAndDelete(req.params.id)
+    .then((exercise) => {
+      if (exercise == null)
+        return res.status(404).json(`Exercise ${req.params.id} not found.`);
+      res.json(exercise);
+    })
+    .catch((err) => res.status(400).json(`Error: ${err}`));
+});
+
+router.put("/update/:id", (req, res) => {
+  Exercise.findById(req.params.id).then((exercise) => {
+    if (exercise == null) return res.status(404).json(`Exercise ${req.params.id} not found.`)
+
+    exercise.username = req.body.username;
+    exercise.description = req.body.description;
+    exercise.duration = Number(req.body.duration);
+    exercise.date = Date.parse(req.body.date);
+
+    exercise.save()
+      .then((added) => res.json(added))
+      .catch((err) => res.status(400).json(`Error: ${err}`));
+  })
+  .catch(err => res.status(400).json(`Error: ${err}`))
+});
+
 module.exports = router;
